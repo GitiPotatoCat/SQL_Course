d0517f4d2c69973952e7e3d76fb2f1a44449bcb4
Majumder, Soumyadip (Cognizant) <2416003@cognizant.com>
2025-12-02 11:04:43 +0530

    Practice Set 1 solution of question set is added. GitIgnore file is added.

---
+++ PracticeSet1.sql
@@ -0,0 +1,21 @@
+-- Literal + SELECT + Statement Practice
+
+-- Solution 1
+SELECT 'Lenovo Thinkpad'
+GO
+
+-- Solution 2
+SELECT 7*4
+GO
+
+-- Solution 3
+SELECT (7-4) * 8
+GO
+
+-- Solution 4
+SELECT 'Brewster''s SQL Training class'
+GO
+
+-- Solution 5
+SELECT 'Day 1 of Training', 5*3
+GO
