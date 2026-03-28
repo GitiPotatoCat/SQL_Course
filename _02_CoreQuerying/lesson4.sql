c23ebb9ec344625fd51495f6726951384a1e5e9c
GitiPotatoCat <Soumyadip.Majumder@cognizant.com>
2026-03-28 11:13:01 +0000

    ORDER BY Concept is added

--- _02_CoreQuerying/lesson4.sql
+++ _02_CoreQuerying/lesson4.sql
@@ -11,3 +11,29 @@ ORDER BY Salary DESC;
 
 
 -- Sort by department A-Z, then salary highest-first within each dept 
+SELECT CONCAT(FirstName, ' ', LastName) AS [Full_Name], 
+    Department, Salary
+FROM dbo.Employees 
+ORDER BY Department ASC, Salary DESC;
+GO 
+
+
+-- You can ORDER BY a column not in the SELECT list 
+SELECT LastName, HireDate
+FROM dbo.Employees 
+ORDER BY HireDate ASC;
+GO 
+
+
+-- You can ORDER BY column position (not recommended in production - fragile) 
+-- we called total 4 columns for section so upto call from (1 - 4) with ASC or DESC
+SELECT FirstName, LastName, Salary, HireDate
+FROM dbo.Employees
+ORDER BY 4 DESC;    -- 4th column = HireDate 
+GO 
+
+SELECT EmployeeID, LastName, Salary
+FROM dbo.Employees
+ORDER BY 3 ASC;     -- 3rd column = Salary
+GO
+
