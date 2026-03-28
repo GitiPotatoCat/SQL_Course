c6866c80650fd8b1857ad00032856320703cc729
GitiPotatoCat <Soumyadip.Majumder@cognizant.com>
2026-03-28 10:41:33 +0000

    docker container yml is added

--- _02_CoreQuerying/lesson4.sql
+++ _02_CoreQuerying/lesson4.sql
@@ -5,6 +5,12 @@ GO
 
 
 -- Sort by salary, highest first 
-SELECT CONCAT() 
+SELECT CONCAT(FirstName, ' ', LastName) AS [Full_Name] , Salary, Department
 FROM dbo.Employees 
 ORDER BY Salary DESC; 
+
+
+-- Sort by department A-Z, then salary highest-first within each dept 
+SELECT FirstName
+FROM dbo.Employees 
+ORDER BY Department ASC, Salary DESC;
